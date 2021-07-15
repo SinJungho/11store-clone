caec81b9ccb11c89eae7683d8c89233bc8a568ba
SinJungho <zmtls0516@naver.com>
2021-07-15 17:07:57 +0900

    main 아이콘 완성

--- slide.js
+++ slide.js
@@ -49,3 +49,22 @@ function showSlides(n) {
   slides[n].style.display = "block";
   dots[n].className += " active";
 }
+
+// nav fix
+function navigo (){
+  const header = document.querySelector('header');
+  const headerheight = header.clientHeight;
+  document.addEventListener('scroll', onScroll, { passive: true });
+
+function onScroll () {
+  const scrollposition = pageYOffset;
+  const nav = document.querySelector('nav');
+   if (headerheight<=scrollposition){
+     nav.classList.add('fix')
+   }
+   else {
+     nav.classList.remove('fix');
+   }
+ } 
+  
+}
