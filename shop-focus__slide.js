95c2a5b06c3cc1c2d73a400a221cf70a8e23e54c
SinJungho <zmtls0516@naver.com>
2021-07-16 16:35:55 +0900

    시선 집중 영역 완성 (header 이미지 슬라이드 수정 필요)

--- shop-focus__slide.js
+++ shop-focus__slide.js
@@ -1,62 +1,62 @@
-var slideIndex = 0; //slide index
+var slideIndex_shop = 0; //slide index
 
 // HTML 로드가 끝난 후 동작
 window.onload=function(){
-  showSlides(slideIndex);
+  showSlides_shop(slideIndex_shop);
 
   // Auto Move Slide
   var sec = 3000;
   setInterval(function(){
-    slideIndex++;
-    showSlides(slideIndex);
+    slideIndex_shop++;
+    showSlides_shop(slideIndex_shop);
 
   }, sec);
 }
 
 
 // Next/previous controls
-function moveSlides(n) {
-  slideIndex = slideIndex + n
-  showSlides(slideIndex);
+function moveSlides_shop(n) {
+  slideIndex_shop = slideIndex_shop + n
+  showSlides_shop(slideIndex_shop);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  slideIndex = n;
-  showSlides(slideIndex);
+  slideIndex_shop = n;
+  showSlides_shop(slideIndex_shop);
 }
 
-function showSlides(n) {
+function showSlides_shop(n) {
 
-  var slides = document.getElementsByClassName("mySlides");
+  var slides_shop = document.getElementsByClassName("mySlides__shop-focus");
   var dots = document.getElementsByClassName("dot");
-  var size = slides.length;
+  var size = slides_shop.length;
 
   if ((n+1) > size) {
-    slideIndex = 0; n = 0;
+    slideIndex_shop = 0; n = 0;
   }else if (n < 0) {
-    slideIndex = (size-1);
+    slideIndex_shop = (size-1);
     n = (size-1);
   }
 
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  for (i = 0; i < slides_shop.length; i++) {
+      slides_shop[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[n].style.display = "block";
-  dots[n].className += "active";
+  slides_shop[n].style.display = "block";
+  dots[n].className += " active";
 }
 
 // nav fix
-function navigo (){
+function navigo_shop (){
   const header = document.querySelector('header');
   const headerheight = header.clientHeight;
   document.addEventListener('scroll', onScroll, { passive: true });
 
-function onScroll () {
+function onScroll_shop () {
   const scrollposition = pageYOffset;
   const nav = document.querySelector('nav');
    if (headerheight<=scrollposition){
